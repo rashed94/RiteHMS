86e9ec161ced4e4e18ca42c98fce5540370940a2
Muzahidul Islam <Muzahidul Islam>
2016-05-29 16:34:19 +0600

    Invoice initiated

--- App/HMS.View/ClientCode/Script/Controller/BillingController.js
+++ App/HMS.View/ClientCode/Script/Controller/BillingController.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-HmsApp.controller("BillingController", function ($scope, $routeParams, BillingService) {
+HmsApp.controller("BillingController", function ($scope, $routeParams, $modal, BillingService) {
     $scope.GetBillingItemByPatientId = function (patientId) {
         BillingService.GetBillingItemByPatientId(patientId)
             .success(function (pt) {
@@ -12,6 +12,37 @@ HmsApp.controller("BillingController", function ($scope, $routeParams, BillingSe
                 console.log($scope.status);
             });
     }
+
+    $scope.InvoiceModal = function (size, isEdit) {
+        var billingItems = [
+            {
+                ItemId: 112,
+                Amount: 400
+            },
+            {
+                ItemId: 114,
+                Amount: 120
+            }
+        ];
+        var modalInstance = $modal.open({
+            templateUrl: '/ClientCode/Template/Invoice.html',
+            size: size,
+            controller: 'InvoiceModalController',
+            scope: $scope,
+            resolve: {
+                billingItems: function () {
+                    return isEdit ? $scope.BillingItems : billingItems;
+                }
+            }
+        });
+        modalInstance.result.then(function (result) {
+            $scope.BillingItem = result.BillingItem;
+            $scope.SaveInvoice();
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
 
     //$scope.GetBillingItemByPatientId($scope.Patiend.Id);
     var tabClass = ".summary";
