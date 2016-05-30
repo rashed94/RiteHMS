27acd646cff36b307ded7ef04a50620299c08b1f
Rashidul Alam <rashed94@gmail.com>
2016-05-30 19:55:37 +0600

    UI update

---
+++ App/DAL.EF/Repository/Impl/PatientInvoiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HMS.Model.Core;
+using System.Linq;
+
+namespace HMS.DAL.Repository
+{
+    public class PatientInvoiceRepository : Repository<PatientInvoice>, IPatientInvoiceRepository
+    {
+        public PatientInvoiceRepository()
+        {
+            _DbSet = _DbContext.Set<PatientInvoice>();
+        }
+
+    }
+}
